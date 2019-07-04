6c7a489803873b9c40870943035326c0e835d181
KasiaKoper <katarzynakoper2@gmail.com>
2019-07-04 18:43:16 +0200

    home controller added

---
+++ src/main/java/com/example/workload/controller/HomeController.java
@@ -0,0 +1,4 @@
+package com.example.workload.controller;
+
+public class HomeController {
+}
--- src/main/java/com/example/workload/controller/MainController.java
+++ src/main/java/com/example/workload/controller/MainController.java
@@ -1,10 +1,19 @@
 package com.example.workload.controller;
 
-//
+
+import com.example.workload.model.Employee;
+import com.example.workload.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 //@Controller
 //@RequestMapping("/actions")
 public class MainController {
-//
+
 //    @Autowired
 //    private EmployeeRepository employeeRepository;
 //
